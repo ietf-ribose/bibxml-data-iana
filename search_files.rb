b53e56444b41d3fae3ffeadf9801a72f097e5b51
Andrei Kislichenko <android.2net@gmail.com>
2021-11-26 13:35:27 +0100

    testing API

--- search_files.rb
+++ search_files.rb
@@ -5,6 +5,6 @@ page = 1
 uri = URI "https://api.github.com/search/code?q=<registry+repo:ietf-ribose/iana-registries+extension:xml&page=#{page}"
 resp = Net::HTTP.get uri
 json = JSON.parse resp
-puts "Items: #{json['items'].size}"
+puts "Items: #{json['items'][0].inspect}"
 puts "total_count: #{json['total_count']}"
 puts "incomplete_results: #{json['incomplete_results']}"
