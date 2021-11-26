0ae893f05e221cb9567cf9951cd93ae1057e2773
Andrei Kislichenko <android.2net@gmail.com>
2021-11-26 19:38:41 +0100

    testing API

--- search_files.rb
+++ search_files.rb
@@ -5,11 +5,13 @@ def fetch(page = 1)
   uri = URI "https://api.github.com/search/code?q=<registry+repo:ietf-ribose/iana-registries+extension:xml&page=#{page}"
   # headers = { 'Authorization' => "token #{ENV['GITHUB_TOKEN']}" }
   req = Net::HTTP::Get.new uri
-  req['authorization'] = "Bearer #{ENV['GITHUB_TOKEN']}"
+  req['authorization'] = "Bearer #{ENV['GITHUB_TOKEN']}" if ENV['GITHUB_TOKEN']
   resp = Net::HTTP.start(uri.hostname, uri.port, use_ssl: true) do |http|
     http.request req
   end
   json = JSON.parse resp.body
+  puts "Retry-After: #{resp['Retry-After']}" if resp['Retry-After']
+  puts resp.to_hash
   puts "Page: #{page}; message: #{json['message']}; documentation_url: #{json['documentation_url']}"
   # puts json.keys
   # puts "items_number: #{json['items'].size}"
@@ -25,5 +27,4 @@ def fetch(page = 1)
   end
 end
 
-puts "Token: #{ENV['GITHUB_TOKEN']}"
 fetch
