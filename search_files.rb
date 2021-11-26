3d41a50bccc40b522c60eebb0546de6e9acf34c6
Andrei Kislichenko <android.2net@gmail.com>
2021-11-26 14:18:09 +0100

    testing API

--- search_files.rb
+++ search_files.rb
@@ -3,7 +3,8 @@ require 'json'
 
 def fetch(page = 1)
   uri = URI "https://api.github.com/search/code?q=<registry+repo:ietf-ribose/iana-registries+extension:xml&page=#{page}"
-  resp = Net::HTTP.get uri, headers: { 'Authorization' => "token #{ENV['GITHUB_TOKEN']}" }
+  headers = { 'Authorization' => "token #{ENV['GITHUB_TOKEN']}" }
+  resp = Net::HTTP.get uri, headers: headers
   json = JSON.parse resp
   puts "Page: #{page}; message: #{json['message']}; documentation_url: #{json['documentation_url']}"
   # puts json.keys
