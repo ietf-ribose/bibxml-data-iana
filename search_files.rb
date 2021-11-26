fbd0486799b40780e72e95ef449c6f539b3d0075
Andrei Kislichenko <android.2net@gmail.com>
2021-11-26 14:06:07 +0100

    testing API

--- search_files.rb
+++ search_files.rb
@@ -1,14 +1,21 @@
 require 'net/http'
 require 'json'
 
-page = 1
-uri = URI "https://api.github.com/search/code?q=<registry+repo:ietf-ribose/iana-registries+extension:xml&page=#{page}"
-resp = Net::HTTP.get uri
-json = JSON.parse resp
-puts "items_number: #{json['items'].size}"
-puts "total_count: #{json['total_count']}"
-puts "incomplete_results: #{json['incomplete_results']}"
-puts 'Items:'
-json['items'].each do |item|
-  puts item['name']
+def fetch(page = 1)
+  uri = URI "https://api.github.com/search/code?q=<registry+repo:ietf-ribose/iana-registries+extension:xml&page=#{page}"
+  resp = Net::HTTP.get uri
+  json = JSON.parse resp
+  puts "Page: #{page}; message: #{json['message']}; documentation_url: #{json['documentation_url']}"
+  # puts json.keys
+  # puts "items_number: #{json['items'].size}"
+  # puts "total_count: #{json['total_count']}"
+  # puts "incomplete_results: #{json['incomplete_results']}"
+  # puts 'Items:'
+  # json['items'].each do |item|
+  #   puts item['name']
+  # end
+  fetch(page + 1) if (json['total_count'].to_i - (page * 30)).positive?
 end
+
+puts "Token: #{ENV['GITHUB_TOKEN']}"
+fetch
